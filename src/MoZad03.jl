717b050f8aa7fe9164cca296caf18d2385ca96a4
Konrad Mielnik <konrad_mielnik@wp.pl>
2020-05-05 21:03:12 +0200

    Adding function and tests

--- src/MoZad03.jl
+++ src/MoZad03.jl
@@ -1,5 +1,32 @@
 module MoZad03
 
-greet() = print("Hello World!")
+function zad22(funk,xL,xU;ep=1e-8,golden=0.618)
+              fa=funk(xL)
+              fb=funk(xU)
+              x1=xU-((xU-xL)*(golden))
+              x2=xL+((xU-xL)*(golden))
+              err=Inf
+              while err>ep
+                  if funk(x2)<funk(x1)
+                      xL=x1
+                      x1=x2
+                      x2=xU-((xU-xL)*(golden))
+                  elseif funk(x2)>funk(x1)
+                      xU=x2
+                      x2=x1
+                  x1=xL+((xU-xL)*(golden))
+                  else
+                      xL=(x1+x2)/2
+                      xU=xL
+                  end
+                 err=2*abs((xU-xL)/(xU+xL))
+                  end
+              Xmin=(x1+x2)/2
+              print("Minimum: ",round(funk(Xmin),digits=2), " for: ", Xmin, " x1: " ,round(x1,digits=2), " x2: ", round(x2,digits=2))
+
+       return(round(funk(Xmin),digits=2),round(Xmin,digits=2))
+       end
+	   
+export zad22
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,15 @@
 using MoZad03
 using Test
 
-@testset "MoZad03.jl" begin
-    # Write your own tests here.
+@testset "testy " begin
+
+	funk,xL,xU = x->x^2,-400,400
+	@test zad22(funk,xL,xU) == (0,0)
+	
+	funk,xL,xU = x->sin(x),0.1,pi
+	@test zad22(funk,xL,xU) == (0.0,3.14)
+	
+	funk,xL,xU = x->x^6+x^4+x^5+x^2+x ,-2, 2
+	@test zad22(funk,xL,xU) == (-0.21,-0.34)
+	
 end
